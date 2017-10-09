4149520d6c6b38b8cc4955d7565071b3563278e6
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-10-09 12:28:42 +0100

    AMD more complex example

--- slow-loading-modules/main.js
+++ slow-loading-modules/main.js
@@ -1,7 +1,9 @@
 // global define
 define = require(__dirname+"/amd.js");
 
-define(["week-days"], function(weekDay) {
+define(["week-days", "tutu"], function(weekDay, Tutu) {
+  console.log(Tutu);
+  console.log(weekDay.chuchu);
   console.log(weekDay.name(0));
 });
 
--- slow-loading-modules/week-days.js
+++ slow-loading-modules/week-days.js
@@ -1,8 +1,9 @@
-define([], function() {
+define(["chuchu"], function(Chuchu) {
   var names = ["Sunday", "Monday", "Tuesday", "Wednesday",
                "Thursday", "Friday", "Saturday"];
   return {
     name: function(number) { return names[number]; },
-    number: function(name) { return names.indexOf(name); }
+    number: function(name) { return names.indexOf(name); },
+    chuchu: Chuchu
   };
 });
